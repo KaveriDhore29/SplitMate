4e5c6adc16733f415417eeaa8e933e2bdd8d167a
Shravan_Shetty_19045 <shravan.shetty@bts.com>
2024-12-04 00:53:02 +0530

    Create group updates.

--- splitmate/src/app/group-details/group-details.component.ts
+++ splitmate/src/app/group-details/group-details.component.ts
@@ -13,17 +13,25 @@ export class GroupDetailsComponent implements OnInit{
   groupName : string ='';
   membersNames: any[] = [];
   showPopup: boolean = false;
+  username: string='';
+  totalGroupExpenses: number = 0;
+  totalAmountOwed: number = 0;
+  value  :string='';
 
   constructor(private route: ActivatedRoute, private dataService: DataService) { }
 
   ngOnInit(): void {
+    
     this.route.paramMap.subscribe(params => {
       const newGroupId = params.get('id');
       if (newGroupId) {
         this.groupId = newGroupId;
         this.loadGroupDetails();
+      this.getUserFromSessionStorage();
+      this.senddata();
       }
     });
+  
   }
 
 
@@ -41,14 +49,45 @@ export class GroupDetailsComponent implements OnInit{
           name: member.username,  // or use the appropriate field for the name
           email: member.email
         }));
-
+        this.calculateTotalExpenses();
         console.log('Group Name:', this.groupName);
         console.log('Members Names:', this.membersNames);
+        
       },
       (error) => {
         console.error('Error fetching group details:', error);
       }
     );
+  }
+  getUserFromSessionStorage() {
+    const userString = sessionStorage.getItem('loggedInUser');
+    if (userString) {
+      const user = JSON.parse(userString);
+      this.username = user.given_name;
+      console.log("heheh"+ this.username);
+      
+    }
+  }
+  calculateTotalExpenses(): void {
+    // Calculate total group expenses
+    this.totalGroupExpenses = this.groupDetails[0].expenses.reduce(
+      (total: number, expense: any) => total + expense.amount, 0
+    );
+
+    // Calculate total amount owed (example calculation)
+    this.totalAmountOwed = this.calculateTotalOwed(this.groupDetails[0].expenses);
+  }
+
+  calculateTotalOwed(expenses: any[]): number {
+    // Implement your specific logic to calculate total amount owed
+    // This is a placeholder and should be replaced with your actual business logic
+    return expenses.reduce((total, expense) => {
+      // Example simplified calculation
+      return total + (expense.amount / expense.members.length);
+    }, 0);
+  }
+  senddata(){
+    
   }
 }
 
