97c1bffd993d68ba883b461ee03c66b274f3dd85
kaveri <dhorekaveri@gmail.com>
2024-12-04 12:22:37 +0530

    added interceptors

--- splitmate/src/app/auth-interceptor.interceptor.ts
+++ splitmate/src/app/auth-interceptor.interceptor.ts
@@ -8,11 +8,19 @@ import {
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class MyInterceptorInterceptor implements HttpInterceptor {
+export class AuthInterceptorInterceptor implements HttpInterceptor {
 
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = sessionStorage.getItem('authToken');
+    if (token) {
+      const clonedRequest = request.clone({
+        headers: request.headers.set('Authorization', `Bearer ${token}`)
+      });
+      return next.handle(clonedRequest);
+    }
     return next.handle(request);
   }
 }
+
